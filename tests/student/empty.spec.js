d796a34915175f0a70c166ee91eb152ef97af393
murad <shaikhs.murad@gmail.com>
2026-02-22 21:52:56 +0500

    empty

---
+++ tests/student/empty.spec.js
@@ -0,0 +1,8 @@
+// empty.spec.js
+const { test, expect } = require('../fixtures/screenshotFixture');
+
+test.describe('Empty', () => {
+  test('placeholder', async ({ page }) => {
+    // Add your test steps here
+  });
+});
